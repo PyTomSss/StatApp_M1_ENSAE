4a27eb6e0aa8f869efbe995acc8fb0da5a1c3a90
Julien L <julien.leclerc@hec.edu>
2024-03-06 10:56:20 +0100

    push_correction

--- brouillon_julien/modele_avance/regression_linaire_avancee.py
+++ brouillon_julien/modele_avance/regression_linaire_avancee.py
@@ -8,8 +8,17 @@ signals_is, assets_is = matrices_in_sample[0], matrices_in_sample[1]
 
 def estimer_beta(signals, assets):
     lr = LinearRegression()
-    lr.fit(signals, assets)
+    lr.fit(assets, signals)
     return lr.coef_
 
 estimated_beta = estimer_beta(signals_is, assets_is)
 
+portfeuille = signals_is@estimated_beta
+rendements_par_jour_par_actifs = assets_is*portfeuille
+rendements_par_jour = np.mean(rendements_par_jour_par_actifs, axis=1)
+
+def sharpe_ratio(rendements_par_jour):
+    rendement_moyen = np.mean(rendements_par_jour)
+    volatilite_portefeuille = np.std(rendements_par_jour)
+    sharpe_ratio = rendement_moyen/volatilite_portefeuille
+    return sharpe_ratio
